f440ed54de421b4edd5a2a610bad5de727053f76
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-22 19:05:56 -0800

    adds signin route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -22,11 +22,11 @@ userRouter.post('/signup', async (req, res) => {
     return
   }
 
-  const exisitingUser = await User.findOne({
+  const userExists = await User.findOne({
     username: req.body.username,
   })
 
-  if (exisitingUser) {
+  if (userExists) {
     res.status(411).json({
       message: 'Email already taken',
     })
@@ -40,8 +40,46 @@ userRouter.post('/signup', async (req, res) => {
     },
     JWT_SECRET
   )
+
   res.status(200).json({
     message: 'User created succesfully',
+    token,
+  })
+})
+
+const signinSchema = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+})
+userRouter.post('/signin', async (req, res) => {
+  const success = signinSchema.safeParse(req.body)
+  if (!success) {
+    res.status(411).json({
+      message: 'Error while logging in',
+    })
+    return
+  }
+
+  const userExists = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  })
+
+  if (!userExists) {
+    res.status(411).json({
+      message: 'Error while logging in',
+    })
+    return
+  }
+
+  const token = jwt.sign(
+    {
+      username: userExists.username,
+    },
+    JWT_SECRET
+  )
+
+  res.status(200).json({
     token,
   })
 })
