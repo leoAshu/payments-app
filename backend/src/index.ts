3ad1f760a38c63c7612f10fcdbc9ed841f714f58
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-22 18:32:44 -0800

    adds cors, bodyparser, jwt secret and an example env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import Express from 'express'
 import dotenv from 'dotenv'
 import v1Router from './routes'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 dotenv.config()
+
 const app = Express()
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.use('/api/v1', v1Router)
 
