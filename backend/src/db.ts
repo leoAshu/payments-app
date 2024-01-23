22640f3eaab36e5d0a8541e0a47fa54abf501c24
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-22 18:11:54 -0800

    defines userschema and runs a server

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,13 +3,42 @@ import mongoose from 'mongoose'
 
 dotenv.config()
 
-const connectDB = async () => {
-  // if (process.env.DB_URL) {
-  //   await mongoose.connect(process.env.DB_URL)
-  //   console.log('DB connected')
-  // } else {
-  //   console.log('DB url not defined')
-  // }
+interface User {
+  username: string
+  fistName: string
+  lastName: string
+  password: string
 }
 
-export { connectDB }
+const UserSchema = new mongoose.Schema<User>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  fistName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export { User }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,9 @@
 import Express from 'express'
 import dotenv from 'dotenv'
-import { connectDB } from './db'
 
 dotenv.config()
 const app = Express()
 
 app.listen(process.env.PORT, () => {
-  connectDB()
   console.log(`Server is running on port: ${process.env.PORT}`)
 })
