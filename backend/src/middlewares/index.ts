d76b202b4d4a090fa9c3dff4e2a673bdcc9ea061
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-22 19:21:03 -0800

    creates an authentication middleware

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { JWT_SECRET } from '../config'
+
+interface AuthRequest extends Request {
+  userId?: string
+}
+
+const authMiddleWare = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    res.status(403).json({})
+    return
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload
+    req.userId = decoded.userId
+    next()
+  } catch (err) {
+    res.status(403).json({})
+  }
+}
