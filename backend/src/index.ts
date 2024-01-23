1cabee1f6e1fa87deaeddf36e7aecaebea75297e
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-22 18:21:25 -0800

    exposes a default router for /ap1/v1 route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
 import Express from 'express'
 import dotenv from 'dotenv'
-import mainRouter from './routes'
+import rootRouter from './routes'
 
 dotenv.config()
 const app = Express()
 
-app.use('/api/v1', mainRouter)
+app.use('/api/v1', rootRouter)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`)
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 
-const mainRouter = Router()
+const rootRouter = Router()
 
-mainRouter.get('/', (req, res) => {
+rootRouter.get('/', (req, res) => {
   res.status(200).json({ message: 'Server is live!' })
 })
 
-export default mainRouter
+export default rootRouter
