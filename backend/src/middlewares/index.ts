c4b149ac696271f5ebda5f4e561d82f980accbe5
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-22 19:22:08 -0800

    creates an authentication middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -18,9 +18,13 @@ const authMiddleWare = (req: AuthRequest, res: Response, next: NextFunction) => 
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload
-    req.userId = decoded.userId
-    next()
+
+    if (decoded.userId) {
+      req.userId = decoded.userId
+      next()
+    }
   } catch (err) {
     res.status(403).json({})
+    return
   }
 }
