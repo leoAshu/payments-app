80a7d481060f26bee31a5751e1480cefafe68568
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-24 05:01:18 -0800

    implements funds transfer flow

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,4 +1,7 @@
-import { useEffect } from 'react'
+import axios from 'axios'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { userAtom } from '../store/atoms'
 
 interface ModalProps {
   isOpen: boolean
@@ -8,7 +11,61 @@ interface ModalProps {
   closeModal: () => void
 }
 
-const Modal = ({ isOpen, closeModal, firstName, lastName }: ModalProps) => {
+const Modal = ({ isOpen, closeModal, userId, firstName, lastName }: ModalProps) => {
+  const [error, setError] = useState('')
+  const [amount, setAmount] = useState('')
+  const [success, setSuccess] = useState('')
+  const setUser = useSetRecoilState(userAtom)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setError('')
+    setSuccess('')
+    setAmount(e.target.value)
+  }
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    try {
+      const token = localStorage.getItem(import.meta.env.VITE_APP_LOCAL_STORAGE_KEY)
+
+      if (!token) {
+        return
+      }
+
+      const response = await axios.post(
+        `${import.meta.env.VITE_APP_API_BASE_URL}/account/transfer`,
+        {
+          to: userId,
+          amount: Number(amount),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+
+      setSuccess(response.data.message)
+      setUser((prevUserData) => {
+        const newUser = {
+          ...prevUserData,
+        }
+        newUser.balance = newUser.balance - Number(amount)
+        return newUser
+      })
+      setAmount('')
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        setError(err.response?.data.message)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   const handleKeyPress = (event: KeyboardEvent) => {
     if (event.key === 'Escape') {
       closeModal()
@@ -32,6 +89,18 @@ const Modal = ({ isOpen, closeModal, firstName, lastName }: ModalProps) => {
           >
             <div className="text-2xl font-semibold">Send Money</div>
 
+            {Boolean(success) && (
+              <div className="bg-gray-50 w-full mt-4 text-sm pl-2 py-2 border-l-8 border-green-500 flex items-center">
+                {success}
+              </div>
+            )}
+
+            {Boolean(error) && (
+              <div className="bg-gray-50 w-full mt-4 text-sm pl-2 py-2 border-l-8 border-red-500 flex items-center">
+                {error}
+              </div>
+            )}
+
             <div className="mt-10 flex w-full justify-start items-center">
               <div className="w-10 h-10 rounded-full bg-green-500 text-white font-semibold flex justify-center items-center">
                 {firstName.charAt(0).toUpperCase()}
@@ -42,20 +111,23 @@ const Modal = ({ isOpen, closeModal, firstName, lastName }: ModalProps) => {
               </div>
             </div>
 
-            <form className="mt-6 flex flex-col w-full">
+            <form className="mt-6 flex flex-col w-full" onSubmit={handleSubmit}>
               <label className="mb-2 text-xs font-semibold">Amount (in USD)</label>
               <input
                 type="number"
                 className="bg-white border border-gray-300 text-gray-900 placeholder:text-gray-500 outline-none focus:border-black text-sm rounded-md px-3 py-2"
                 placeholder="Enter amount"
                 name="amount"
+                value={amount}
+                onChange={onChangeHandler}
                 required
               />
 
               <input
                 type="submit"
-                className="mt-6 bg-green-500 text-white py-2 rounded-md text-sm font-medium"
-                value="Initiate Transfer"
+                className="mt-6 bg-green-500 text-white py-2 rounded-md text-sm font-medium disabled:bg-opacity-75"
+                disabled={isLoading}
+                value={isLoading ? 'Please wait...' : 'Initiate Transfer'}
               />
             </form>
           </div>
