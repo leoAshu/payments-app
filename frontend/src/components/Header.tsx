be7f72d69658b680ead55472c598325bf8efe830
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-24 00:37:44 -0800

    updates header to show user name

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,16 @@
+import { useRecoilValueLoadable } from 'recoil'
+import { userAtom } from '../store/atoms'
+
 const Header = () => {
+  const user = useRecoilValueLoadable(userAtom)
+
   return (
     <header className="shadow-md py-3 px-4">
       <div className="md:max-w-4xl xl:max-w-7xl mx-auto flex justify-between items-center">
         <div className="text-lg font-semibold">Payments App</div>
 
         <div className="flex items-center">
-          <div className="mr-3 font-medium">Hello, User</div>
+          <div className="mr-3 font-medium">Hello, {user.contents.firstName}</div>
           <div className="bg-gray-200 w-8 h-8 rounded-full flex justify-center items-center font-medium">U</div>
         </div>
       </div>
