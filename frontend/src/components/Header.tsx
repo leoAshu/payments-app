60491b4bfbc68137f3e40fc761ce02a6fe0a2266
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-24 01:15:11 -0800

    implements logout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,20 @@
-import { useRecoilValueLoadable } from 'recoil'
+import { useRecoilState } from 'recoil'
 import { userAtom } from '../store/atoms'
 
 const Header = () => {
-  const user = useRecoilValueLoadable(userAtom)
+  const [user, setUser] = useRecoilState(userAtom)
+
+  const logout = () => {
+    localStorage.removeItem(import.meta.env.VITE_APP_LOCAL_STORAGE_KEY)
+
+    setUser({
+      userId: '',
+      username: '',
+      firstName: '',
+      lastName: '',
+      balance: 0,
+    })
+  }
 
   return (
     <header className="shadow-md py-2 px-4">
@@ -10,10 +22,13 @@ const Header = () => {
         <div className="text-lg font-semibold">Payments App</div>
 
         <div className="flex items-center">
-          <div className="mr-3 font-medium">Hello, {user.contents.firstName}</div>
-          <div className="bg-gray-200 w-10 h-10 rounded-full flex justify-center items-center font-medium">
-            {user.contents.firstName.charAt(0)}
-            {user.contents.lastName.charAt(0)}
+          <div className="mr-3 font-medium">Hello, {user.firstName}</div>
+          <div
+            onClick={logout}
+            className="bg-gray-200 w-10 h-10 rounded-full flex justify-center items-center font-medium select-none cursor-pointer"
+          >
+            {user.firstName.charAt(0)}
+            {user.lastName.charAt(0)}
           </div>
         </div>
       </div>
