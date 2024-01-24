1c77dae5e5d7b98caf76baef304f6c4dfa1b1df0
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-24 05:36:14 -0800

    sets default balance to be integer

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -51,7 +51,7 @@ userRouter.post('/signup', async (req, res) => {
 
   await Account.create({
     userId: newUser._id,
-    balance: 1 + Math.floor(Math.random()) * 100000,
+    balance: Math.floor(1 + Math.random() * 1000) * 10,
   })
 
   const token = jwt.sign(
